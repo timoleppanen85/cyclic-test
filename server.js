3e57fe60fc08db5b435c5b842f26e5b4aef214d5
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2024-04-17 00:44:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ const userModel = require("./models/user");
 const sessionModel = require("./models/session");
 
 let app = express();
-//MOI
+
 const mongo_url = process.env.MONGODB_URL;
 const mongo_user = process.env.MONGODB_USERNAME;
 const mongo_password = process.env.MONGODB_PASSWORD;
 
-const url = "mongodb+srv://" + mongo_user + ":" + mongo_password + "@" + mongo_url + "/shoppingdatabase?retryWrites=true&w=majority"
+const url = "mongodb+srv://" + mongo_user + ":" + mongo_password + "@" + mongo_url + "/shopping?retryWrites=true&w=majority"
 
 app.use("/", express.static("public"));
-
-//MOI
 
 app.use(express.json());
 
